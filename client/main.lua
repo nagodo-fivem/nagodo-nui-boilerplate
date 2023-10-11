b9d9cf9fd814923ef7d6ef36cd7dce07dc1ceec4
Nagodo <magnusschoendorff@gmail.com>
2023-10-11 19:17:31 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,21 @@
+local translationsSet = false
+
 function ShowNUI() 
     SetNuiFocus(true, true)
     SendNUIMessage({
         action = "openmenu",
         data = ""
     })
+end
+
+function SetTranslations()
+    if not translationsSet then
+        SendNUIMessage({
+            action = "setTranslations",
+            data = Locale[Config.Language]
+        })
+        translationsSet = true
+    end
 end
 
 -- NUI Callbacks --
